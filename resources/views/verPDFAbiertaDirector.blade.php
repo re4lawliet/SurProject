51b72c6b55b40e5f1bcf2d1b00a043834485c5ea
fish <re4lawliet@gmail.com>
2019-01-31 18:57:04 -0600

    comit 31-1-2019 6:56 pm Director enviar ordenes abiertas

---
+++ resources/views/verPDFAbiertaDirector.blade.php
@@ -0,0 +1,29 @@
+<!-- ENCABEZADO -->
+@extends('layouts.appDirector')
+@section('content')
+    <center>
+        <h1> Vista Previa Abono A Orden Abierta y Cotizacion</h1>
+        <br>
+        <h3> Orden de Compra</h3>
+        @foreach($orden as $o)
+        <div class="container">
+            <embed src="/{{ $o->pdf }}" type="application/pdf" width="100%" height="1150px">
+        </div>
+        @endforeach
+        <br>
+        <br>
+        <h3> Cotizacion</h3>
+        <div class="container">
+            <embed src="/{{ Session::get('pdf_presupuesto') }}" type="application/pdf" width="100%" height="1150px">
+        </div>
+
+
+
+        <div class="form-group">
+            <a href="#">
+                <button name="btn_Orden" id="btn_Orden"  type="submit" class="btn btn-success" onclick="location.href='/enviar_correoA'">APROBADO Y ENVIADO</button> 
+            </a>
+        </div>
+
+    </center>
+@endsection
