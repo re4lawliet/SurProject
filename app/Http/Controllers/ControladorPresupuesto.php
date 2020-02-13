e3cdf6eb445db9d75a43ddabb8fc5768edc67148
Alan Guzman <hadamir.95@gmail.com>
2020-02-12 23:02:02 -0600

    dos decimales en ControladorPresupuesto

--- app/Http/Controllers/ControladorPresupuesto.php
+++ app/Http/Controllers/ControladorPresupuesto.php
@@ -189,7 +189,7 @@ class ControladorPresupuesto extends Controller
 
             //Todas las compras del proyecto
             //$compras = DB::select("SELECT p.id as id_proyecto, p.nombre_proyecto, pa.id as id_partida, pa.nombre as nombre_partida, (o.total * o.tasa_cambio) as total, e.nombre_empresa, s.titulo_solicitud
-            $compras = DB::select("SELECT o.fecha_creacion, o.no_orden, e.nombre_empresa, s.titulo_solicitud, (o.total * o.tasa_cambio) as total, (o.pagado * o.tasa_cambio) as pagado, ((o.total * o.tasa_cambio) - (o.pagado * o.tasa_cambio)) as saldo, p.id as id_proyecto, pa.id as id_partida
+            $compras = DB::select("SELECT o.fecha_creacion, o.no_orden, e.nombre_empresa, s.titulo_solicitud, ROUND((o.total * o.tasa_cambio),2) as total, ROUND((o.pagado * o.tasa_cambio),2) as pagado, ROUND(((o.total * o.tasa_cambio) - (o.pagado * o.tasa_cambio)),2) as saldo, p.id as id_proyecto, pa.id as id_partida
                                     FROM proyectos as p, partidas as pa, solicitudes as s, empresas as e, orden as o
                                     WHERE p.id = $idProyecto
                                     AND o.id_proyecto = $idProyecto
